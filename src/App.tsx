2ccac809be0ceaa6efc765a989610faf4ad7b10b
17639638825 <562985990@qq.com>
2023-04-04 10:28:44 +0800

    创建挂载点

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>111</div>;
+  return (
+    <div>
+      <h1>注册子应用</h1>
+      <div id="microSlot"></div>
+      <div id="microSlot1"></div>
+    </div>
+  );
 }
 
 export default App;
